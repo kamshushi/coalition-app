175d75c7ed9862a9a787b87b03fa27ea6c4df1dd
kamshushi <ahmed.elkamshoushy7@yahoo.com>
2024-06-23 21:24:23 +0300

    initial home page setup

---
+++ src/components/DiagnosisHistory/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DiagnosisHistory = () => {
+  return <div>DiagnosisHistory</div>;
+};
+
+export default DiagnosisHistory;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./styles.module.css";
+const Home = () => {
+  return <div className={styles.container}>Home</div>;
+};
+
+export default Home;
