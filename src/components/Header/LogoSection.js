bb210dc53d18d62b1214ac2c3feb07bf7043d2e5
kamshushi <ahmed.elkamshoushy7@yahoo.com>
2024-06-24 23:15:23 +0300

    nav ui changes

--- src/components/Header/LogoSection.js
+++ src/components/Header/LogoSection.js
@@ -3,7 +3,7 @@ import React from "react";
 const LogoSection = () => {
   return (
     <div>
-      <img src="/AppLogo.png" alt="app-logo" />;
+      <img src="/AppLogo.png" alt="app-logo" />
     </div>
   );
 };
